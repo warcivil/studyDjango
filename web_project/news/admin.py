bfad9aaf7e39be48296278cae072ca753f740d0d
warcivil <36387132+warcivil@users.noreply.github.com>
2021-04-10 23:34:45 +0500

    task14

--- web_project/news/admin.py
+++ web_project/news/admin.py
@@ -6,7 +6,7 @@ class NewsAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'category', 'created_at', 'updated_at', 'photo', 'is_published']
     list_display_links=['id', 'title']
     search_fields =('title', 'content')
-    list_editable = ('is_published')
+    list_editable = ('is_published',)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['id', 'title']
     list_display_links=['id', 'title']
