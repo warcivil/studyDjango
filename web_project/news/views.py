fe4e802653b0c69cd3e99db0a28d4644ad660088
warcivil <36387132+warcivil@users.noreply.github.com>
2021-05-07 08:13:57 +0500

    task20

--- web_project/news/views.py
+++ web_project/news/views.py
@@ -5,11 +5,9 @@ from .models import News, Category
 
 def index(request):
     news = News.objects.all()
-    categories = Category.objects.all()
     context = {
         'news': news,
         'title': 'список новостей',
-        'category': categories,
     }
     return render(request, 'news/index.html', context)
 
@@ -18,6 +16,5 @@ def test(request):
 
 def get_category(request, index_category):
     news = News.objects.filter(category_id=index_category)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=index_category)
-    return render(request, 'news/category.html', {'news': news, 'category': category, 'categoryDb': categories})
+    return render(request, 'news/category.html', {'news': news, 'category': category})
